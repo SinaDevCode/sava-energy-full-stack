7f1b4ff00934622eaea8da01fbc60707c292229e
Sina Salimirad <168358448+SinaDevCode@users.noreply.github.com>
2024-09-15 13:56:50 +0330

    fixed the bugs of responsive navbar

--- frontend/src/pages/Root.jsx
+++ frontend/src/pages/Root.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import { FaBars } from "react-icons/fa6";
 import "../styles/root.css";
 
@@ -36,32 +36,37 @@ const menuLink = menuLinks.titles.map((title, index) => (
 
 const Root = () => {
 	return (
-		<div className="sticky top-0 left-0 w-full max-h-28 flex justify-between items-center z-10 bg-light">
-			<video
-				className="w-44"
-				muted
-				autoPlay
-			>
-				<source src="videos/logoMotion/sava-energy-logo-motion-500x200.webm" />
-				Your browser doesn't support video
-			</video>
-			<div className="h-full flex flex-col justify-between items-end gap-2">
-				<Link
-					to="/Login"
-					className="w-32 m-2 py-2 text-white text-sm font-bold text-center rounded-lg transition bg-gold hover:bg-cyan"
+		<>
+			<div className="sticky top-0 left-0 w-full max-h-28 flex justify-between items-center z-10 bg-light">
+				<video
+					className="w-44"
+					muted
+					autoPlay
 				>
-					Login
-				</Link>
-				<div className="Navbar max-h-[52px] flex justify-between items-center relative pl-2 lg:pl-4 pr-4 lg:pr-8 bg-darkblue xs:min-w-[16rem] md:min-w-[44rem] lg:min-w-[54rem]">
-					<ul className="flex">{menuLink}</ul>
-					<FaBars
-						size="18px"
-						color="#fff"
-						className="MenuButton cursor-pointer flex md:hidden m-4"
-					/>
+					<source src="videos/logoMotion/sava-energy-logo-motion-500x200.webm" />
+					Your browser doesn't support video
+				</video>
+				<div className="h-full flex flex-col justify-between items-end gap-2">
+					<Link
+						to="/Login"
+						className="w-32 m-2 py-2 text-white text-sm font-bold text-center rounded-lg transition bg-gold hover:bg-cyan"
+					>
+						Login
+					</Link>
+					<div className="Navbar max-h-[52px] flex justify-between items-center relative pl-2 lg:pl-4 pr-4 lg:pr-8 bg-darkblue xs:min-w-[16rem] md:min-w-[44rem] lg:min-w-[54rem]">
+						<ul className="flex">{menuLink}</ul>
+						<FaBars
+							size="18px"
+							color="#fff"
+							className="MenuButton cursor-pointer flex md:hidden m-4"
+						/>
+					</div>
 				</div>
 			</div>
-		</div>
+			<div id="content">
+				<Outlet />
+			</div>
+		</>
 	);
 };
 
--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -1,10 +1,25 @@
-// Import page files here
+// Import Main Page Files Here
 import Root from "../pages/Root";
+
+import ContactUs from "../pages/ContactUs";
+
+// Import 404 Error Handler Page
+import NotFound from "../pages/NotFound";
 
 const routes = [
 	{
 		path: "/",
 		element: <Root />,
+		children: [
+			{
+				path: "ContactUs",
+				element: <ContactUs />,
+			},
+		],
+	},
+	{
+		path: "*",
+		element: <NotFound />,
 	},
 ];
 
