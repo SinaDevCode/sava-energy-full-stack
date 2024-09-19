e8e00876526e3a9cee7f5f784dc9ed1e22bc2355
Sina Salimirad <168358448+SinaDevCode@users.noreply.github.com>
2024-09-19 22:57:13 +0330

    Update NotFound.jsx

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -6,13 +6,13 @@ export default function ErrorPage() {
 	return (
 		<div
 			id="error-page"
-			className="p-8 w-full h-screen bg-light flex flex-col justify-center items-start"
+			className="p-8 w-full h-screen bg-light flex flex-col justify-center items-start text-grey"
 		>
-			<h2 className="text-9xl font-bold text-grey">404</h2>
-			<h3 className="text-6xl font-bold text-grey">
+			<h2 className="text-9xl font-bold">404</h2>
+			<h3 className="text-6xl font-bold">
 				Oops!, this page <br /> isn't available
 			</h3>
-			<p className="font-medium mt-2 mb-10 text-grey">
+			<p className="font-medium mt-2 mb-10">
 				The page you were looking for doesn't exist or has been moved
 			</p>
 			<Link
