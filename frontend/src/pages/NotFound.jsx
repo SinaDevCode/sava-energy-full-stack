09733b07e55683cf33b437f8e0470dc2d72c8600
Sina Salimirad <168358448+SinaDevCode@users.noreply.github.com>
2024-09-19 22:30:23 +0330

    not found page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,26 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../styles/notFound.css";
+
 export default function ErrorPage() {
 	return (
-		<div id="error-page">
-			<h1>Oops!</h1>
-			<p>404</p>
+		<div
+			id="error-page"
+			className="p-8 w-full h-screen bg-light flex flex-col justify-center items-start"
+		>
+			<h2 className="text-9xl font-bold text-grey">404</h2>
+			<h3 className="text-6xl font-bold text-grey">
+				Oops!, this page <br /> isn't available
+			</h3>
+			<p className="font-medium mt-2 mb-10 text-grey">
+				The page you were looking for doesn't exist or has been moved
+			</p>
+			<Link
+				to="/"
+				className="Button"
+			>
+				Back to Home Page
+			</Link>
 		</div>
 	);
 }
