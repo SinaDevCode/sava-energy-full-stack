030a7371c1113aa8f338f53fbaf4c2b111c68471
Sina Salimirad <168358448+SinaDevCode@users.noreply.github.com>
2024-09-19 22:55:15 +0330

    make the Root page components base

--- frontend/src/pages/ContactUs.jsx
+++ frontend/src/pages/ContactUs.jsx
@@ -10,45 +10,9 @@ import { BsMailbox2Flag } from "react-icons/bs";
 const ContactUs = () => {
 	return (
 		<>
-			<div className="flex flex-col xs:p-6 sm:p-10 md:p-20 gap-5 text-grey">
-				<Title title={`Contact Us`} />
-				<p>
-					<span className="font-semibold">SAVA ENERGY&nbsp;</span>
-					began operations on 2021. While the company is new to the
-					industry the staff is comprised of some of the most seasoned
-					industry.
-				</p>
-				<p>
-					SAVA Energy goal is to ensure your success, and to help you
-					become better equipped to meet the future. Therefore, the
-					focus at SAVA Energy has been on rigorous internal R&D to
-					develop cost effective environmentally friendly processes
-					for producing technically complex products of the highest
-					quality
-				</p>
-				<p>
-					SAVA continuously works to improve our products. Secured
-					access to high-grade raw materials and significant
-					investments in energy recovery are ways SAVA works to
-					achieve this. We believe that safe and environmentally
-					responsible production is the only way forward and that
-					together we can develop solutions to many of the world’s
-					challenges.
-				</p>
-				<p>
-					Our <span className="font-semibold">R&D team&nbsp;</span>
-					consist of several graduate chemical engineers and some
-					experience oil field persons that have worked many years to
-					optimize properties in all kind of different downhole
-					conditions, fine tuning setting times, viscosity, density
-					and other important parameters. Checking compatibilities
-					with various fluids.
-				</p>
-			</div>
-
 			<Map />
-
-			<div className="flex flex-col xs:p-6 sm:p-10 md:p-20 gap-5 text-grey">
+			<div className="flex flex-col py-20 xs:px-6 sm:px-12 md:px-24 lg:px-36 gap-5 text-grey">
+				<Title title={`Contact Us`} />
 				<ContactInfo
 					icon={<IoMail title="Email" />}
 					href="https://mail.google.com/mail/?view=cm&fs=1&to=Info@savaenergyco.com"
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,29 +1,36 @@
 import React from "react";
+import About from "../layouts/About";
 
 import Typewriter from "typewriter-effect";
 
 const Home = () => {
 	return (
-		<div className="relative w-full h-[75vh] flex justify-center items-center">
-			<Typewriter
-				onInit={(typewriter) => {
-					typewriter
-						// .pauseFor(2500)
-						.typeString("A simple yet powerful native javascript")
-						.pauseFor(300)
-						.deleteChars(10)
-						.typeString(
-							"<strong>JS</strong> plugin for a cool typewriter effect and "
-						)
-						.typeString(
-							'<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
-						)
-						.pauseFor(1000)
-						.start();
-				}}
-				options={{ wrapperClassName: "text-3xl" }}
-			/>
-		</div>
+		<>
+			<About />
+
+			<div className="relative w-full h-[75vh] flex justify-center items-center">
+				<Typewriter
+					onInit={(typewriter) => {
+						typewriter
+							// .pauseFor(2500)
+							.typeString(
+								"A simple yet powerful native javascript"
+							)
+							.pauseFor(300)
+							.deleteChars(10)
+							.typeString(
+								"<strong>JS</strong> plugin for a cool typewriter effect and "
+							)
+							.typeString(
+								'<strong>only <span style="color: #27ae60;">5kb</span> Gzipped!</strong>'
+							)
+							.pauseFor(1000)
+							.start();
+					}}
+					options={{ wrapperClassName: "text-3xl" }}
+				/>
+			</div>
+		</>
 	);
 };
 
